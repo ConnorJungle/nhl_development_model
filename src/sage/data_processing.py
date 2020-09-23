8d41a94885846d2a4fbc70a57de219019cbdff8e
Connor Jung <connor.jung@2k.com>
2020-09-23 18:48:55 -0400

    for sagemaker

---
+++ src/sage/data_processing.py
@@ -0,0 +1,312 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import numpy as np
+import warnings
+import torch
+import re
+
+warnings.filterwarnings('ignore')
+
+pd.set_option('max_column', 0)
+
+def is_forward(val):
+
+    match = re.search('[c,w,lw,rw,f]', val.lower())
+
+    if match:
+        return 1
+    else:
+        return 0
+
+def get_primary_position(val):
+
+    match = re.search('[c,w,lw,rw,f]', val.lower())
+
+    if match:
+        return 'F'
+    else:
+        return 'D'
+
+def is_drafted(df):
+
+    # if a player goes into that season drafted
+    df['is_drafted'] = np.where(df.start_year >= df.draft_year,
+                                1,
+                                0)
+
+    return df
+
+def extract_rankings_from_text(text):
+    try:
+        text_string=re.findall("[0-9]+",text)
+        rankings=list(map(int,text_string))
+        avg_rankings=sum(rankings)/len(rankings)
+        return int(avg_rankings)
+    except TypeError:
+        return np.NaN
+
+def draft_current_year(df):
+    
+    df = df.set_index(['playerid', 'player', 'year'])
+    
+    df['assigned_draft_rank'] = df.apply(lambda x: extract_rankings_from_text(x.rankings),axis=1)
+    
+    df['assigned_draft_round'] = pd.cut(df.assigned_draft_rank, 
+                                    bins=[p for p in range(1, 219, 31)],
+                                    labels=[r for r in range(1, 8, 1)],
+                                    right=False)
+    
+    df['draft_round'] = np.where(df.draft_round.isnull(),
+                                df.assigned_draft_rank,
+                                df.draft_round)  
+    
+    df['draft_pick'] = np.where(df.draft_pick.isnull(),
+                               df.assigned_draft_round,
+                               df.draft_pick)  
+    
+    return df.drop(columns=['assigned_draft_rank', 'assigned_draft_round']).reset_index()
+    
+
+def get_season_age(df):
+    '''Return the first year a player is NHL draft eligible'''
+
+    df.set_index('playerid', inplace=True)
+
+    # years = df.year.apply(lambda x: int(x.split('-')[1]))
+
+    # create a series filled with valueus of Birth year + 18 on September 15th
+    end_of_year = pd.to_datetime(dict(year=df.end_year,
+                                      month=np.full((len(df), ), 12),
+                                      day=np.full((len(df), ), 31)))
+
+    end_of_year.index = df.index
+
+    # check if player will be 18 years old by September 15th of draft year
+    df['season_age'] = (((end_of_year - df['date_of_birth']) /
+                         np.timedelta64(1, 'Y')) // 1).astype(int)
+
+    df['real_season_age'] = (
+        (end_of_year - df['date_of_birth']) / np.timedelta64(1, 'Y')).round(2)
+
+    return df.reset_index()
+
+
+def collapse_player_league_seasons(df):
+
+    df.sort_values('gp', ascending=False, inplace=True)
+
+    return df.drop_duplicates(subset=['playerid', 'player', 'season_age'], keep='first').sort_values('season_age')
+
+def collapse_player_team_seasons(df):
+
+    df_ = df.groupby(['playerid', 'player', 'position',
+                      'league', 'year']).apply(lambda x: pd.Series({
+                          'team': '/'.join(x.team),
+                          'teamids': np.array(x.teamid),
+                          'season_age': x.season_age.max(),
+                          'real_season_age': x.real_season_age.max(),
+                          'start_year': x.start_year.max(),
+                          'end_year': x.end_year.max(),
+                          'draft_year': x.draft_year.max(),
+                          'draft_year_eligible': x.draft_year_eligible.max(),
+                          'gp': x.gp.sum(),
+                          'g': x.g.sum(),
+                          'a': x.a.sum(),
+                          'tp': x.tp.sum(),
+                          'gpg': x.g.sum() / x.gp.sum(),
+                          'apg': x.a.sum() / x.gp.sum(),
+                          'ppg': x.tp.sum() / x.gp.sum(),
+                          'perc_team_g': (x.perc_team_g * x.gp).sum() / x.gp.sum(),
+                          'perc_team_a': (x.perc_team_a * x.gp).sum() / x.gp.sum(),
+                          'perc_team_tp': (x.perc_team_tp * x.gp).sum() / x.gp.sum(),
+                      }
+                      )).reset_index()
+
+    return df_
+
+def get_next_season_data(df):
+
+    df['next_season'] = df.end_year.astype(str) + '-' + \
+        (df.end_year.astype(int) + 1).astype(str)
+
+    df = df.merge(
+        df[['playerid', 'player', 'year', 'league', 'g', 'a', 'tp',
+            'gpg', 'apg', 'ppg', 'gp']],
+        left_on=['playerid', 'player', 'next_season'],
+        right_on=['playerid', 'player', 'year'],
+        suffixes=('', '_y_plus_1'),
+        how='left'
+    )
+
+    return df
+
+def draft_position(df):
+
+    df['draft_round'] = np.where(df.is_drafted == 1,
+                                 np.where(df.draft_round.notnull(),
+                                          df.draft_round,
+                                          0),
+                                 0
+                                 )
+
+    df['draft_pick'] = np.where(df.is_drafted == 1,
+                                np.where(df.draft_pick.notnull(),
+                                         df.draft_pick,
+                                         0),
+                                0
+                                )
+
+    return df
+
+def prepare_features(df, target, scaler=None):
+
+    features = ['forward', 'gp', 'gp_y_plus_1',
+                'draft_pick', 'is_drafted',
+                'height', 'weight', 'real_season_age',
+                'gpg', 'apg', 'ppg', 'perc_team_g',
+                'perc_team_a', 'perc_team_tp']
+            
+    df = get_next_season_data(df)
+
+    df = df[(df.gp < 85)
+        & (df.season_age >= 17)
+        & (df.season_age <= 24)
+        & (df.gp_y_plus_1.notnull())]
+
+    df = is_drafted(df)
+    df = draft_position(df)
+    df['forward'] = df.position.apply(is_forward)
+    df['real_season_age'] = df['real_season_age'] - \
+        df['season_age']  # scale age based on birthday
+
+    season_age = pd.get_dummies(df.season_age, drop_first=True)
+
+    df.set_index(['playerid', 'player', 'season_age'], inplace=True)
+
+    season_age.index = df.index
+    start_league = pd.get_dummies(df.league, drop_first=True)
+    draft_round = pd.get_dummies(
+        df.draft_round,  drop_first=True, prefix='round')
+
+    X = df[[f for f in features if (target ==
+            'league_y_plus_1' and f != 'gp_y_plus_1') | (target == 'ppg_y_plus_1')]]
+
+    y = df[target]
+    # feature scaling
+    scaler = MinMaxScaler()
+    scaler.fit(X)
+    X[X.columns] = scaler.transform(X)
+        
+    X = X.merge(start_league, left_index=True, right_index=True)\
+        .merge(season_age, left_index=True, right_index=True)\
+        .merge(draft_round, left_index=True, right_index=True)
+
+    if target == 'ppg_y_plus_1':  # if we're predicting performance y+1 then we need league y+1
+
+        next_season_league = pd.get_dummies(
+            df['league_y_plus_1'], drop_first=True, prefix='next_yr')
+
+        X = X.merge(next_season_league, left_index=True, right_index=True)
+    else:
+        y = pd.get_dummies(y, prefix='next_yr')
+
+    return X, y, scaler
+
+def prepare_features_single_season(df, scaler, target):
+
+    features = ['forward', 'gp', 'gp_y_plus_1',
+                'draft_pick', 'is_drafted',
+                'height', 'weight', 'real_season_age',
+                'gpg', 'apg', 'ppg', 'perc_team_g',
+                'perc_team_a', 'perc_team_tp']
+
+    df = df[(df.season_age >= 17)
+            & (df.season_age <= 24)]
+
+    df = is_drafted(df)
+    df = draft_position(df)
+    df['forward'] = df.position.apply(is_forward)
+    df['real_season_age'] = df['real_season_age'] - \
+        df['season_age']  # scale age based on birthday
+
+    season_age = pd.get_dummies(df.season_age)
+
+    df.set_index(['playerid', 'player', 'season_age'], inplace=True)
+
+    season_age.index = df.index
+    start_league = pd.get_dummies(df.league)
+    draft_round = pd.get_dummies(
+        df.draft_round, prefix='round')
+
+    X = df[[f for f in features if (target == 
+            'league_y_plus_1' and f != 'gp_y_plus_1') | (target == 'ppg_y_plus_1')]]
+
+    # feature scaling
+    X[X.columns] = scaler.transform(X)
+
+    X = X.merge(start_league, left_index=True, right_index=True)\
+        .merge(season_age, left_index=True, right_index=True)\
+        .merge(draft_round, left_index=True, right_index=True)
+
+    return X
+
+def column_scaler(df, feature):
+
+    df = get_next_season_data(df)
+
+    df = df[(df.gp < 85)
+        & (df.season_age >= 17)
+        & (df.season_age <= 24)
+        & (df.gp_y_plus_1.notnull())]
+
+    X = df[[feature]]
+
+    scaler = MinMaxScaler()
+    scaler.fit(X)
+
+    return scaler
+
+def generate_players(X, y, indices):
+
+    labels = []
+    sequences = []
+
+    for idx in indices:
+        labels.append(y.loc[idx].values)
+        sequences.append(X.loc[idx].values)
+
+    return sequences, labels
+
+
+def prepare_sequence(seq):
+
+    return seq.index, torch.FloatTensor(seq.values)
+
+
+def pad_data(df, players):
+
+    pad = pd.concat([pd.DataFrame([[i] for i in range(17, 25)],
+                                  columns=['age'],
+                                  index=pd.MultiIndex.from_tuples([player for i in range(17, 25)],
+                                                                  names=['playerid', 'player']))
+                     for player in players]).reset_index()
+
+    padded = pad.merge(df,
+                       left_on=['playerid', 'player', 'age'],
+                       right_on=['playerid', 'player', 'season_age'],
+                       how='left').fillna(-1)
+
+    return padded.drop(columns=['age'])
+
+def pad_sequence(df):
+
+    pad = pd.DataFrame([[i] for i in range(17, 25)],
+                       columns=['age'])
+
+    padded = pad.merge(df,
+                       left_on=['age'],
+                       right_index=True,
+                       how='left').fillna(-1)
+
+    return padded.drop(columns=['age'])
---
+++ src/sage/pytorch_lstm.py
@@ -0,0 +1,219 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import os
+import json
+import argparse
+import logging
+import sys
+
+import optuna
+from optuna.trial import FixedTrial
+
+from data_processing import *
+
+from secrets import * 
+
+import time
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+EPOCHS = 10
+LOG_INTERVAL = 10
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+def get_player_dataset(target='ppg_y_plus_1'):
+
+    df = pd.read_csv(f'{args.data_dir}/player_season_stats.csv')
+
+    X, y,_ = prepare_features(df, target)
+
+    players = X.index.droplevel(-1).unique() # get number indices
+    n_players = players.shape[0] # get number of players
+    train_idx, test_idx = train_test_split(players, test_size=0.3, random_state=18)
+
+    print('--- Padding Player Data ---')
+
+    X = pad_data(X.reset_index(), players)
+    y = pad_data(y.reset_index(), players)
+
+    X.set_index(['playerid', 'player', 'season_age'], inplace=True)
+    y.set_index(['playerid', 'player', 'season_age'], inplace=True)
+
+    print('--- Generating Player Data ---')
+    train_seq, train_target = generate_players(X, y, train_idx)
+    test_seq, test_target = generate_players(X, y, test_idx)
+
+    train_idx_bool = pd.Series(list(X.index.droplevel(-1))).isin(train_idx).values
+    test_idx_bool = pd.Series(list(X.index.droplevel(-1))).isin(test_idx).values
+
+    train_real_values = (X[train_idx_bool] != -1).all(axis=1)
+    test_real_values = (X[test_idx_bool] != -1).all(axis=1)
+
+    return X, y, train_seq, train_target, test_seq, test_target, train_idx, test_idx, train_real_values, test_real_values, train_idx_bool, test_idx_bool
+
+def save_model(model, model_dir, trial_number):
+    logger.info("Saving the model_{}.".format(trial_number))
+    path = os.path.join(model_dir, 'model_{}.pth'.format(trial_number))
+    # recommended way from http://pytorch.org/docs/master/notes/serialization.html
+    torch.save(model.cpu().state_dict(), path)
+    
+def define_model(trial):
+    # We optimize the number of layers, hidden untis and dropout ratio in each layer.
+    lstm_layers = trial.suggest_int("lstm_layers", 2, 4)
+    lstm_hidden = trial.suggest_int("lstm_hidden", low=X.shape[1], high=X.shape[1]*3, step=X.shape[1])
+
+    return Model(lstm_layers=lstm_layers, hidden_size=lstm_hidden)
+
+class Model(nn.Module):
+    def __init__(self, input_size=74, hidden_size=74, lstm_layers=3, output_size=1, drop=0.2):
+        
+        super().__init__()
+        self.start = time.time()
+        self.hidden_size = hidden_size
+        self.lstm_layers = lstm_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, lstm_layers, batch_first=True,
+#                             dropout=drop
+                           )
+        self.linear = nn.Linear(hidden_size, output_size)  
+        self.relu = nn.ReLU()
+
+    def forward(self, seasons):
+
+        ht = torch.zeros(self.lstm_layers, 1, self.hidden_size)   # initialize hidden state
+        ct = torch.zeros(self.lstm_layers, 1, self.hidden_size)  # initialize cell state
+        predictions = torch.Tensor([]) # to store our predictions for season t+1
+        
+        hidden = (ht, ct)
+        
+        for idx, season in enumerate(seasons):  # here we want to iterate over the time dimension
+            lstm_input = torch.FloatTensor(season).view(1,1,len(season)) # LSTM takes 3D tensor
+            out, hidden = self.lstm(lstm_input, hidden) # LSTM updates hidden state and returns output
+            pred_t = self.linear(out) # pass LSTM output through a linear activation function
+            pred_t = self.relu(pred_t) # since performance is non-negative we apply ReLU
+            
+            predictions = torch.cat((predictions, pred_t)) # concatenate all the predictions
+
+        return predictions
+    
+def objective(trial):
+
+    # Generate the model.
+    model = define_model(trial).to(DEVICE)
+
+    # Generate the optimizers.
+    optimizer_name = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD"])
+    lr = trial.suggest_float("lr", 1e-3, 1e-1, log=True)
+    optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
+    
+    loss_fn = nn.MSELoss()
+    
+    # to track the average training loss per epoch as the model trains
+    avg_train_losses = []
+    # to track the average validation loss per epoch as the model trains
+    avg_valid_losses = [] 
+
+    # Training of the model.
+    model.train()
+    for epoch in range(EPOCHS):
+        
+        train_loss = []
+        test_loss = []
+        print(f'Running epoch: {epoch}')
+        for seasons, targets in zip(train_sequences, train_targets): # data is a list returning tuple of X, y
+
+            optimizer.zero_grad()
+
+            mask = torch.tensor([(season > -1).all() for season in seasons]) # create mask for real seasons only      
+            targets = torch.FloatTensor(targets)[mask]
+
+            predictions = model(seasons)    
+            # now here, we want to compute the loss between the predicted values
+            # for each season and the actual values for each season
+            # TO-DO: random select grouth truth or predicted value in next timestep
+            loss = loss_fn(predictions[mask].squeeze(1), targets) 
+            loss.backward()
+            optimizer.step() 
+            train_loss.append(loss.item())
+            
+        avg_train_losses.append(np.nanmean(train_loss))
+            
+    # validate with test set
+    model.eval()
+    with torch.no_grad():
+        for seasons, targets in zip(test_sequences, test_targets):
+            mask = torch.tensor([(season > -1).all() for season in seasons]) # create mask for real seasons only      
+            targets = torch.FloatTensor(targets)[mask]
+
+            predictions = model(seasons)    
+            # now here, we want to compute the loss between the predicted values
+            # for each season and the actual values for each season
+            # TO-DO: random select grouth truth or predicted value in next timestep
+            loss = self.loss_fn(predictions[mask].squeeze(1), targets) 
+            test_loss.append(loss.item())
+
+    
+    print(f'epoch: {ep:3} train avg. loss: {np.nanmean(avg_train_losses):10.8f}')
+    print(f'epoch: {ep:3} test loss: {np.nanmean(test_loss):10.8f}')
+
+    save_model(model, '/tmp', trial.number)
+    
+    trial.set_user_attr('job_name', args.training_env['job_name'])
+    
+    return np.nanmean(avg_train_losses)
+    
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+
+    # To configure Optuna db
+    parser.add_argument('--host', type=str)
+    parser.add_argument('--db-name', type=str, default='optuna')
+    parser.add_argument('--db-secret', type=str, default='demo/optuna/db')
+    parser.add_argument('--study-name', type=str, default='lstm-pytorch-optune')
+    parser.add_argument('--region-name', type=str, default='us-east-1')
+    parser.add_argument('--n-trials', type=int, default=50)
+    
+    # Data, model, and output directories These are required.
+    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
+    parser.add_argument('--training-env', type=str, default=json.loads(os.environ['SM_TRAINING_ENV']))
+    
+    args, _ = parser.parse_known_args()    
+    
+    # Get the player dataset.
+    X, y, train_sequences, train_targets, test_sequences, test_targets, train_idx, test_idx, train_real_values, test_real_values, train_idx_bool, test_idx_bool = get_player_dataset()
+
+    secret = get_secret(args.db_secret, args.region_name)
+    connector = 'pymysql'
+    db = 'mysql+{}://{}:{}@{}/{}'.format(connector, secret['username'], secret['password'], args.host, args.db_name)
+
+    study = optuna.study.load_study(study_name=args.study_name, storage=db)
+    study.optimize(objective, n_trials=args.n_trials)
+
+    logger.info("Number of finished trials: {}".format(len(study.trials)))
+
+    logger.info("Best trial:")
+    trial = study.best_trial
+
+    logger.info("  Value: {}".format(trial.value))
+
+    logger.info("  Params: ")
+    for key, value in trial.params.items():
+        logger.info("    {}: {}".format(key, value))
+        
+    # retrieve and save the best model
+    try:
+        model = define_model(FixedTrial(trial.params)).to(DEVICE)
+        with open(os.path.join('/tmp', 'model_{}.pth'.format(trial.number)), 'rb') as f:
+            model.load_state_dict(torch.load(f))
+            
+        torch.save(model.cpu().state_dict(), os.path.join(args.model_dir, 'model.pth'))
+        torch.save(trial.params, os.path.join(args.model_dir, 'params.pth'))
+        logger.info('    Model saved: model_{}.npz'.format(trial.number))
+    except Exception as e: 
+        logger.info('    Save failed: {}'.format(e))
